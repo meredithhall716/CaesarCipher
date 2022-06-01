558c97eeb2f022d6903b467a62bb3566fe51e7ea
meredithhall716 <meredith.hall.oboe@gmail.com>
2022-06-01 16:39:07 -0400

    first

--- js/caesarCipher.js
+++ js/caesarCipher.js
@@ -1,3 +1,38 @@
-exports.caesarCipher = function() {
+exports.caesarCipher = function(str, num) {
+    let result = []
+  
+    if (num === 0) {
+      return str
+    }
 
-};
+    for (let i of str) {
+        if (!i.match(/[a-zA-Z]/g)){
+            asciiVal = i
+    } 
+        if (i.match(/[a-zA-Z]/g)) {
+            let code = i.charCodeAt(i)
+
+
+            if (code >= 65 && code <= 90) {
+                asciiVal = String.fromCharCode((code + num))
+        } else if (code >= 97 && code <= 122) {
+                asciiVal = String.fromCharCode((code + num))
+        } 
+        
+        if (num > 0){
+            if (asciiVal > 'Z' && asciiVal < 'a' || asciiVal > 'z'){
+                asciiVal = String.fromCharCode(asciiVal.charCodeAt(0) - 26)
+            }
+        
+        } 
+        if (num < 0){
+            if (asciiVal < 'a' && asciiVal > 'Z' || asciiVal < 'A'){
+                asciiVal = String.fromCharCode(asciiVal.charCodeAt(0) + 26)
+            }
+        }
+        
+        }
+    result.push(asciiVal)
+    }
+return result.join("")
+}
